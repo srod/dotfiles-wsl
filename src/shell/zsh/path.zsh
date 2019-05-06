637c93640384aacbd6751c00fea8dd607c1cf644
Rodolphe Stoclin <srod@users.noreply.github.com>
2019-05-06 17:56:23 +0200

    Update path

--- src/shell/zsh/path.zsh
+++ src/shell/zsh/path.zsh
@@ -3,6 +3,9 @@ export PATH="/usr/sbin:/sbin:$PATH"
 
 export PATH="/usr/local/opt/sqlite/bin:$PATH"
 export PATH="/usr/local/opt/curl/bin:$PATH"
+export PATH="/usr/local/opt/grep/libexec/gnubin:$PATH"
+export PATH="/usr/local/opt/vim/bin:$PATH"
+export PATH="/usr/local/opt/wget/bin:$PATH"
 export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
 
 # Load custom commands
