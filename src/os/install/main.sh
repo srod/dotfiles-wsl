c11ddabd22ba671786b9f953eb0649861ab01066
Rodolphe Stoclin <srodolphe@gmail.com>
2019-09-30 20:10:10 +0200

    Update

--- src/os/install/main.sh
+++ src/os/install/main.sh
@@ -2,10 +2,10 @@
 
 print_in_blue "\n • Installs\n\n"
 
-source ./src/os/install/zsh.sh
-source ./src/os/install/nvm.sh
-source ./src/os/install/packages.sh
-source ./src/os/install/ruby.sh
-source ./src/os/install/gem.sh
-source ./src/os/install/npm.sh
-source ./src/os/install/cleanup.sh
+source ./install/zsh.sh
+source ./install/nvm.sh
+source ./install/packages.sh
+source ./install/ruby.sh
+source ./install/gem.sh
+source ./install/npm.sh
+source ./install/cleanup.sh
