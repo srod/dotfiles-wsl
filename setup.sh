6501a0408c3791dce1dc8d9641acfd105c87d6a1
Rodolphe Stoclin <srodolphe@gmail.com>
2019-10-12 17:12:14 +0200

    Update

--- setup.sh
+++ setup.sh
@@ -106,7 +106,7 @@ update_system() {
     ask_for_confirmation "Your system must be updated first, do you want to?"
     if answer_is_yes; then
         execute \
-            "sudo apt update && sudo apt upgrade" "Checking updates..."
+            "sudo apt-get update && sudo apt-get upgrade" "Checking updates..."
     fi
 
 }
